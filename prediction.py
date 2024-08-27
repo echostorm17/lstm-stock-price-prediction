d751c3a27fe8ae193429e0111cc8f3ae8d45bc52
skydev111 <skydev359@outlook.com>
2024-08-26 21:19:01 -0700

    feat: calculate error

--- prediction.py
+++ prediction.py
@@ -69,3 +69,8 @@ trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
 
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
