0a572f0838ca5f9b23c6dc1e35642f180fb457af
skydev111 <skydev359@outlook.com>
2024-08-26 21:11:12 -0700

    feat: get train and test data

--- prediction.py
+++ prediction.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pandas import read_csv
-
+from sklearn.preprocessing import MinMaxScaler
 
 
 
@@ -17,3 +17,16 @@ np.random.seed(5)
 
 # load the dataset
 df = read_csv(input_file, header=None, index_col=None, delimiter=',')
+
+# take close price column[5]
+all_y = df[5].values
+dataset=all_y.reshape(-1, 1)
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets, 50% test data, 50% training data
+train_size = int(len(dataset) * 0.5)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
